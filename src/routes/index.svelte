ff746a6ea5c0b843e20ab58408a0059ea4c6f34b
Tony Sullivan <tony@navillus.nl>
2021-04-24 17:02:17 +0200

    adding homepage hero section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section>
+	<div class="container hero">
+		<h1>
+			Every problem is <strong>different</strong>.
+			<br />
+			Your <strong>solutions</strong> should be too.
+		</h1>
+		<p>
+			Maximize your business potential with performant websites built with the latest, innovative
+			technologies.
+		</p>
+		<a class="btn" href="/services">How we can help</a>
+	</div>
+</section>
