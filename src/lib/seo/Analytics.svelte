862083bdfe0d9012bc3293dcaf5169a50dc3da81
Tony Sullivan <tony@navillus.nl>
2021-06-05 13:03:17 +0200

    feat: :chart_with_upwards_trend: Adding fathom analytics

---
+++ src/lib/seo/Analytics.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import site from '$data/site.json';
+
+	const { id, domain } = site.analytics.fathom;
+
+</script>
+
+<svelte:head>
+	<!-- Fathom - beautiful, simple website analytics -->
+	<script
+		src="https://cdn.usefathom.com/script.js"
+		data-spa="auto"
+		data-site={id}
+		data-included-domains={domain}
+		defer></script>
+	<!-- / Fathom -->
+</svelte:head>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
+	import { dev } from '$app/env';
 	import Footer from '$lib/Footer.svelte';
 	import Nav from '$lib/Nav.svelte';
+	import Analytics from '$lib/seo/Analytics.svelte';
 	import LDTag from '$lib/seo/LDTag.svelte';
 	import { organizationSchema, websiteSchema } from '$utils/json-ld';
 	import '../app.postcss';
 
 </script>
+
+{#if !dev}
+	<Analytics />
+{/if}
 
 <LDTag schema={organizationSchema} />
 <LDTag schema={websiteSchema} />
