83c38606f0f0b13d311ae636d5c653abc5852ac3
Tony Sullivan <tony@navillus.nl>
2021-06-05 16:22:20 +0200

    fix: :bug: Fixing meta tags
    Not sure exactly where they were getting lost in the build...?

--- src/lib/seo/SEO.svelte
+++ src/lib/seo/SEO.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import site from '$data/site.json';
-	import Meta from './Meta.svelte';
 
 	export let title = 'Jamstack Developers for Hire';
 	export let description =
@@ -9,26 +8,32 @@
 	export let image = '/share.png';
 
 	$: url = `${site.url}${$page.path}`;
+	$: imageAbsolute = `${site.url}${image}`;
 
 </script>
 
 <svelte:head>
-	<Meta {title} {description} {image} />
-
+	<title>{title} • Navillus</title>
+	<meta name="description" content={description} />
 	<meta
 		name="keywords"
 		content="web development, consulting, jamstack, serverless, static site, netlify, svelte, software, engineering, software engineering, api development"
 	/>
-
 	<meta name="robots" content="index,follow" />
 	<meta name="author" content={site.author} />
-
 	<link rel="canonical" href={url} />
-	<meta property="og:url" content={url} />
-	<meta name="twitter:url" content={url} />
 
+	<meta property="og:title" content={title} />
+	<meta property="og:site_name" content={title} />
 	<meta property="og:type" content="website" />
+	<meta property="og:description" content={description} />
+	<meta property="og:image" content={imageAbsolute} />
+	<meta property="og:url" content={url} />
 
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+	<meta name="twitter:image" content={imageAbsolute} />
+	<meta name="twitter:url" content={url} />
 	<meta name="twitter:card" content={site.social.twitter_card} />
 	<meta name="twitter:site" content={site.social.twitter} />
 	<meta name="twitter:creator" content={site.social.twitter} />
