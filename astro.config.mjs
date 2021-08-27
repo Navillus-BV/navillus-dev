eaded5f0ce8fa061031f90f7db80f3f999506a7f
Tony Sullivan <tony.f.sullivan@gmail.com>
2021-08-27 18:34:07 +0200

    updating to astro v0.20.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-export default {
+// Full Astro Configuration API Documentation:
+// https://docs.astro.build/reference/configuration-reference
+
+// @type-check enabled!
+// VSCode and other TypeScript-enabled text editors will provide auto-completion,
+// helpful tooltips, and warnings if your exported object is invalid.
+// You can disable this by removing "@ts-check" and `@type` comments below.
+
+// @ts-check
+export default /** @type {import('astro').AstroUserConfig} */ ({
   buildOptions: {
     site: "https://navillus.dev",
     sitemap: true,
@@ -7,4 +16,4 @@ export default {
     port: 8080,
   },
   renderers: [],
-};
+});
