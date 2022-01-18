aaa4e71c4b8ab1e5d011732b35d94094467d7b3b
Tony Sullivan <tony.sullivan@hyperlab.se>
2022-01-18 17:59:00 +0100

    refactor to hook up the blog index pagination

---
+++ src/data/pages/blog.md
@@ -0,0 +1,14 @@
+---
+  template: page
+  permalink: /blog
+  published: true
+  seo:
+    title: Blog
+  hero:
+    title: |-
+      **JAMSTACK DEVELOPERS FOR HIRE**
+    subtitle: |-
+      The Navillus Blog
+    content: |-
+      Exploring the Jamstack and the future of web development.
+---
--- types/cms.d.ts
+++ types/cms.d.ts
@@ -1,130 +1,131 @@
 declare namespace CMS {
   interface Site {
-    title: string;
-    description: string;
-    url: string;
-    domain: string;
-    author: string;
+    title: string
+    description: string
+    url: string
+    domain: string
+    author: string
     social: {
-      twitter: string;
-      twitter_card: string;
-      image: string;
-    };
+      twitter: string
+      twitter_card: string
+      image: string
+    }
     company: {
-      name: string;
-    };
+      name: string
+    }
   }
 
   interface NavigationItem {
-    title: string;
-    href: string;
-    icon?: string;
+    title: string
+    href: string
+    icon?: string
   }
 
   interface Navigation {
-    main: [NavigationItem];
-    legal: [NavigationItem];
-    social: [NavigationItem];
+    main: [NavigationItem]
+    legal: [NavigationItem]
+    social: [NavigationItem]
   }
 
   interface SEO {
-    title?: string;
-    description?: string;
-    image?: string;
+    title?: string
+    description?: string
+    image?: string
   }
 
-  type AuthorId = string;
+  type AuthorId = string
 
   interface Author {
-    first_name: string;
-    last_name: string;
-    url: string;
-    slug: AuthorId;
+    first_name: string
+    last_name: string
+    url: string
+    slug: AuthorId
   }
 
   interface BasePage {
-    template: "legal" | "page" | "post";
-    permalink: string;
-    published: boolean;
+    template: 'legal' | 'page' | 'post'
+    permalink: string
+    published: boolean
     content: {
-      html: string;
-      md: string;
-    };
-    seo?: SEO;
+      html: string
+      md: string
+    }
+    seo?: SEO
   }
 
   interface PostPage extends BasePage {
-    date: string;
-    author: AuthorId;
-    last_modified_at?: string;
-    category?: string;
-    categories?: string[];
-    tags?: string[];
+    date: string
+    author: AuthorId
+    tweetId?: string
+    last_modified_at?: string
+    category?: string
+    categories?: string[]
+    tags?: string[]
   }
 
   interface ContentPage extends BasePage {
-    template: "page";
-    hero?: Hero;
-    blocks: Block[];
+    template: 'page'
+    hero?: Hero
+    blocks: Block[]
   }
 
   interface LegalPage extends BasePage {
-    template: "legal";
-    last_modified_at?: string;
-    title: string;
+    template: 'legal'
+    last_modified_at?: string
+    title: string
   }
 
-  type Page = ContentPage | LegalPage | PostPage;
+  type Page = ContentPage | LegalPage | PostPage
 
   interface Link {
-    href: string;
-    text: string;
+    href: string
+    text: string
   }
 
   interface Hero {
-    title: string;
-    subtitle: string;
-    content: string;
-    cta: Link;
+    title: string
+    subtitle: string
+    content: string
+    cta: Link
   }
 
   interface UIBlock {
-    title?: string;
-    subtitle?: string;
-    alt?: boolean;
-    id?: string;
+    title?: string
+    subtitle?: string
+    alt?: boolean
+    id?: string
   }
 
   interface BadgesBlockItem {
-    title: string;
-    icon: string;
+    title: string
+    icon: string
   }
 
   interface BadgesBlockGroup {
-    title: string;
-    items: [BadgesBlockItem];
+    title: string
+    items: [BadgesBlockItem]
   }
 
   interface BadgesBlock extends UIBlock {
-    template: "badgesblock";
-    groups: [BadgesBlockGroup];
+    template: 'badgesblock'
+    groups: [BadgesBlockGroup]
   }
 
   interface ContactBlock extends UIBlock {
-    template: "contactblock";
-    content: string;
+    template: 'contactblock'
+    content: string
   }
 
   interface FeaturesBlockItem {
-    icon: string;
-    title: string;
-    content: string;
+    icon: string
+    title: string
+    content: string
   }
 
   interface FeaturesBlock extends UIBlock {
-    template: "featuresblock";
-    items: [FeaturesBlockItem];
+    template: 'featuresblock'
+    items: [FeaturesBlockItem]
   }
 
-  type Block = BadgesBlock | ContactBlock | FeaturesBlock;
+  type Block = BadgesBlock | ContactBlock | FeaturesBlock
 }
